a53e4694b3280b2766dc43fe08c8c61abf1a3218
zzrpatrick <zzrpatrick@users.noreply.github.com>
2019-07-25 12:01:24 +0800

    July 25th work

--- webpage_frontend/web/src/App.js
+++ webpage_frontend/web/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+//core
+import React from 'react';
+
+
+//css
+import './App.css';
+import 'antd/dist/antd.css';
+
+//component
+import Topbanner from "./component/HomePage/Topbanner.jsx"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Topbanner></Topbanner>
     </div>
+
   );
 }
 
---
+++ webpage_frontend/web/src/component/HomePage/Topbanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+export default class Topbanner extends React.Component {
+    render() {
+        return (
+            <Row>
+                <Col span={6}>
+
+                </Col>
+                <Col span={6}>
+                    
+                </Col>
+            </Row>
+        );
+    }
+}
